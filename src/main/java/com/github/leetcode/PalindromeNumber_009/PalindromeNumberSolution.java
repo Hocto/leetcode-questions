daebdc32bd655b8942278a3fdbc9d14e4de98baa
Hocto <orkun.cavdar@gmail.com>
2022-05-11 23:05:49 +0300

    palindrome number

---
+++ src/main/java/com/github/leetcode/PalindromeNumber_009/PalindromeNumberSolution.java
@@ -0,0 +1,17 @@
+package com.github.leetcode.PalindromeNumber_009;
+
+public class PalindromeNumberSolution {
+    public boolean isPalindrome2(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
