5c992cc170b0740b66a77c35876c2a61f15cc15a
Hocto <orkun.cavdar@gmail.com>
2023-10-06 17:32:40 +0300

    jump game solution

---
+++ src/main/java/com/github/leetcode/JumpGame_055/JumpGameSolution.java
@@ -0,0 +1,18 @@
+package com.github.leetcode.JumpGame_055;
+
+public class JumpGameSolution {
+
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
